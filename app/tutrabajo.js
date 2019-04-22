563ccadeac703d6b7ebaf6192686033cb15f5277
andresfeliper3 <andresfeliper.2001@gmail.com>
2019-04-22 15:24:24 -0500

    desing_form1

---
+++ app/tutrabajo.js
@@ -0,0 +1,12 @@
+﻿var modelos = document.getElementById('models_div');
+var form1 = document.getElementById('form1');
+ 
+ function hacerclick() {
+ 	modelos.style.display = "none";
+ 	form1.style.display = "inline-block";
+ }
+
+ function atras() {
+ 	modelos.style.display= "inline-block";
+ 	form1.style.display = "none";
+ }
