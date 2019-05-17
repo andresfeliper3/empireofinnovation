acc1a5b0417b84449ee22f6b8d2c7d6829a2f8a3
andresfeliper3 <andresfeliper.2001@gmail.com>
2019-05-16 19:36:06 -0500

    storage

--- app/tutrabajo.js
+++ app/tutrabajo.js
@@ -30,15 +30,72 @@ var form2 = document.getElementById('form2');
   /* Firebase*/
   var formulario1 = document.getElementById('formulario1');
   var formulario2 = document.getElementById('formulario2');
+
   /*Firebase: formulario buscamos modelos*/
   
 formulario1.addEventListener('submit',(e) => {
 	e.preventDefault();
-	db.collection("monos_modelos").add({
+	var docRef = db.collection("monos_modelos").add({
     nombre: formulario1.name.value,
     email: formulario1.email.value
 	})
+  .then(function(docRef) {
+    key = docRef.id
+    console.log("Document written with ID: ", key);
+    
+})
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
 });
+});
+ 
+//Firebase: storage buscamos modelos
+//Multiple files
+  const storage = firebase.storage();
+  const storageRef = storage.ref();
+  var InputPhoto = document.getElementById('InputPhoto');
+
+  InputPhoto.addEventListener('change', function(e) {
+     
+      //Get files
+     for (var i = 0; i < e.target.files.length; i++) {
+        var imageFile = e.target.files[i];
+         uploadImageAsPromise(imageFile); //Function uploads every single file
+        
+      }
+  });
+  function send() {
+    db.collection("monos_modelos").doc(key).get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data().email);
+        userEmail = doc.data().email;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+  }
+  function uploadImageAsPromise (imageFile) {
+    
+    return new Promise(function (resolve, reject) {
+      //Upload file
+      
+        console.log(userEmail)
+        var modelsRef = storage.ref('BuscamosModelos/'+userEmail+'/'+imageFile.name);
+        var task = modelsRef.put(imageFile);
+    });
+
+  };
+        /*URGENTE poner el orden de los botones así:
+          Enviar
+          Enviar foto
+          Seleccionar
+          Enviar todo y recargar
+
+        */
+      
 
  /*Firebase: formulario crea tu diseño */
 
@@ -49,3 +106,4 @@ formulario1.addEventListener('submit',(e) => {
     email: formulario2.email.value
 	})
 });
+
